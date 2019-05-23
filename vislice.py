6aa5b261b89dd08f3cbf57a0274e992078fd15c9
Kumpilumpi <jakakump@gmail.com>
2019-05-23 23:12:34 +0200

    Uvajanje spletnega vmesnika, bottle

--- vislice.py
+++ vislice.py
@@ -1,9 +1,12 @@
 import Bottle
 import model
 
+Bottle.TEMPLATE_PATH.insert(0, 'C:\\Users\\jakak\\Desktop\\Git\\Vislice\\datoteke\\views')
+
 vislice = model.Vislice()
 
 @Bottle.get('/')
+
 def index():
     return Bottle.template('index.tpl')
 
